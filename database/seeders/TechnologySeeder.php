b7a552ef6431618fa8f9b65ddc08c4d5616826f1
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-27 15:20:11 +0200

    EDIT Seeder to put data into Table "Technologies"

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +14,20 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
+
+    // Uso un Array predefinito di Tipologie per popolare la Tabella Types 
     public function run()
     {
-        //
+        $technologies = ['HTML', 'JavaScript', 'PHP', 'AutoCad', 'Photoshop', 'Adobe Illustrator', 'Excel', 'Word', 'PowerPoint'];
+
+        foreach ($technologies as $technology) {
+
+            $tech = new Technology();
+
+            $tech->name = $technology;
+            $tech->slug = Str::slug($tech->name, '-');
+
+            $tech->save();
+        }
     }
 }
